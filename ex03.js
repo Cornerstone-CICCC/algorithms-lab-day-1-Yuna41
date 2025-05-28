960014a1f0176a340bdb8bb19579085ce20bd718
Yuna Shimonosono <uncnprs@gmail.com>
2025-05-27 19:02:54 -0700

    ex03 completed

--- ex03.js
+++ ex03.js
@@ -1,7 +1,10 @@
 // Exercise 3: Write a function named 'getFullNames' that takes an array of user objects (each with 'firstName' and 'lastName' properties) and returns an array of full names (e.g., 'John Doe'). 
 
 function getFullNames(users) {
-  // your code here
+  const fullName = []
+  for(let i = 0; i < users.length; i++){
+    fullName.push(`${users[i].firstName} ${users[i].lastName}`)
+  }
+  return fullName
 }
-
 console.log(getFullNames([{ firstName: 'John', lastName: 'Doe' }, { firstName: 'Jane', lastName: 'Doe' }])) // ['John Doe', 'Jane Doe']
