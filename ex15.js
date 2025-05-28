b6b6bca51f4de7b6ebfdf7f1132ba4619a40aa24
Yuna Shimonosono <uncnprs@gmail.com>
2025-05-28 08:34:34 -0700

    ex15 completed

--- ex15.js
+++ ex15.js
@@ -1,7 +1,9 @@
 // Exercise 15: Write a function named 'countWords' that takes a string of text and returns the number of words in the string.
 
 function countWords(text) {
-  // your code here
+  const words = text.split(' ')
+  const num = words.length
+  return num
 }
 
 console.log(countWords('The quick brown fox jumps over the lazy dog')) // 9
