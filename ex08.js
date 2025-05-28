e81928940393387792e7adba684e38bef1f64021
Yuna Shimonosono <uncnprs@gmail.com>
2025-05-28 08:09:01 -0700

    ex08 completed

--- ex08.js
+++ ex08.js
@@ -1,7 +1,15 @@
 // Exercise 8: Write a function named 'longestWord' that takes a string of text and returns the longest word in the string.
 
 function longestWord(text) {
-  // your code here
+  let words = text.split(' ')
+  // console.log(words)
+  let longest = ""
+  for(let i=0; i<words.length; i++){
+    // console.log(words[i])
+    if(words[i].length > longest.length){
+      longest = words[i]
+    }
+  }
+  return longest
 }
-
 console.log(longestWord('The duck loves bananas and grapes')) // 'bananas'
